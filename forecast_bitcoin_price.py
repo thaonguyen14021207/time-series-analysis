015b098c08260467ab9290fa9f705800d399243e
thaonguyen14021207 <68944055+thaonguyen14021207@users.noreply.github.com>
2022-10-19 12:45:58 +0700

    Add files via upload

--- forecast_bitcoin_price.py
+++ forecast_bitcoin_price.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 28 11:11:06 2021
-@author: sadrachpierre
-"""
 import pandas as pd 
 import pandas_datareader as web 
 import datetime
@@ -19,13 +13,13 @@ pd.set_option('display.max_rows', None)
 
 
 
-# btc = web.get_data_yahoo(['BTC-USD'], start=datetime.datetime(2018, 1, 1), end=datetime.datetime(2020, 12, 2))
+btc = web.get_data_yahoo(['BTC-USD'], start=datetime.datetime(2018, 1, 2), end=datetime.datetime(2021, 12, 31))
 
-# btc = btc['Close']
+btc = btc['Close']
 
-# btc.to_csv("btc.csv")
+btc.to_csv("data/btc.csv")
 
-btc = pd.read_csv("btc.csv")
+btc = pd.read_csv("data/btc.csv")
 
 
 btc.index = pd.to_datetime(btc['Date'], format='%Y-%m-%d')
@@ -37,10 +31,11 @@ plt.ylabel('BTC Price')
 plt.xlabel('Date')
 plt.xticks(rotation=45)
 plt.plot(btc.index, btc['BTC-USD'], )
+plt.title("BTC Data 2018-2021")
 plt.show()
 
-train = btc[btc.index < pd.to_datetime("2020-11-01", format='%Y-%m-%d')]
-test = btc[btc.index >= pd.to_datetime("2020-11-01", format='%Y-%m-%d')]
+train = btc[btc.index < pd.to_datetime("2021-11-01", format='%Y-%m-%d')]
+test = btc[btc.index >= pd.to_datetime("2021-11-01", format='%Y-%m-%d')]
 print(test)
 plt.plot(train, color = "black", label = 'Training')
 plt.plot(test, color = "red", label = 'Testing')
@@ -48,10 +43,11 @@ plt.ylabel('BTC Price')
 plt.xlabel('Date')
 plt.xticks(rotation=45)
 plt.title("Train/Test split for BTC Data")
+plt.show()
 
 y = train['BTC-USD']
 
-ARMAmodel = SARIMAX(y, order = (1, 0, 1))
+ARMAmodel = SARIMAX(y, order = (5, 5, 5))
 ARMAmodel = ARMAmodel.fit()
 
 y_pred = ARMAmodel.get_forecast(len(test.index))
@@ -59,6 +55,8 @@ y_pred_df = y_pred.conf_int(alpha = 0.05)
 y_pred_df["Predictions"] = ARMAmodel.predict(start = y_pred_df.index[0], end = y_pred_df.index[-1])
 y_pred_df.index = test.index
 y_pred_out = y_pred_df["Predictions"] 
+plt.plot(train, color = "black", label = 'Training')
+plt.plot(test, color = "red", label = 'Testing')
 plt.plot(y_pred_out, color='green', label = 'ARMA Predictions')
 plt.legend()
 
@@ -72,7 +70,7 @@ print("ARMA RMSE: ",arma_rmse)
 
 
 
-ARIMAmodel = ARIMA(y, order = (5, 4, 2))
+ARIMAmodel = ARIMA(y, order = (4, 2, 4))
 ARIMAmodel = ARIMAmodel.fit()
 
 y_pred = ARIMAmodel.get_forecast(len(test.index))
@@ -92,7 +90,7 @@ print("ARIMA RMSE: ",arma_rmse)
 
 
 
-SARIMAXmodel = SARIMAX(y, order = (5, 4, 2), seasonal_order=(2,2,2,12))
+SARIMAXmodel = SARIMAX(y, order = (1, 0, 1), seasonal_order=(2,2,2,12))
 SARIMAXmodel = SARIMAXmodel.fit()
 
 y_pred = SARIMAXmodel.get_forecast(len(test.index))
@@ -101,11 +99,14 @@ y_pred_df["Predictions"] = SARIMAXmodel.predict(start = y_pred_df.index[0], end 
 y_pred_df.index = test.index
 y_pred_out = y_pred_df["Predictions"] 
 plt.plot(y_pred_out, color='Blue', label = 'SARIMA Predictions')
+plt.ylabel('BTC Price')
+plt.xlabel('Date')
+plt.title("BTC Price Prediction")
 plt.legend()
 
 
-import numpy as np
-from sklearn.metrics import mean_squared_error
+# import numpy as np
+# from sklearn.metrics import mean_squared_error
 
-arma_rmse = np.sqrt(mean_squared_error(test["BTC-USD"].values, y_pred_df["Predictions"]))
-print("SARIMA RMSE: ",arma_rmse)
+# arma_rmse = np.sqrt(mean_squared_error(test["BTC-USD"].values, y_pred_df["Predictions"]))
+# print("SARIMA RMSE: ",arma_rmse)
